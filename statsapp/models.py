809a5fbc75640e419ceb7138bd5601299f46a18e
Javohir <codialstudent2007@gmail.com>
2023-06-03 15:57:31 +0500

    2-commit

--- statsapp/models.py
+++ statsapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from userapp.models import Ombor
+from asosiy.models import Mahsulot , Mijoz
 
-# Create your models here.
+class Statistika(models.Model):
+    mahsulot = models.ForeignKey(Mahsulot, on_delete= models.SET_NULL, null= True)
+    mijoz = models.ForeignKey(Mijoz, on_delete= models.SET_NULL, null= True)
+    ombor = models.ForeignKey(Ombor, on_delete= models.SET_NULL, null= True)
+    sana = models.DateTimeField(auto_now_add= True)
+    miqdor = models.PositiveSmallIntegerField()
+    umumiy_summa = models.PositiveIntegerField()
+    tolandi = models.PositiveIntegerField()
+    nasiy = models.PositiveIntegerField()
+
+    def save(self,*args,**kwargs):
+        self.umumiy_summa = int(self.miqdor) * int(self.maxsulot.narx)
+        self.nasiya = self.umumiy_summa - int(self.tolandi)
+        super
